aa116d9250ce14d9c185d22060c4c4aa7dcba7b6
albert bikmukhametov <Albert.Bikmukhametov@tel-int.ru>
2020-11-04 16:03:34 +0300

    py lesson 10

---
+++ python/lessons/lesson-10_if-elif-else.py
@@ -0,0 +1,10 @@
+age = 3
+
+if (age <= 4):
+    print("you are baby")
+elif (age > 4) and (age < 12):
+    print("you are kid")
+elif (age >=12) and (age <19):
+    print("you are teen")
+else:
+    print("you are old")
